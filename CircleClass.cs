597b82e2274d3e30654aafb2f8207dd31a072918
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-21 16:56:13 -0400

    First Commit

---
+++ CircleClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNine
+{
+    class CircleClass
+    {
+        private double radius;
+        private double area;
+        private double circumference;
+        
+        public CircleClass (double radiusInput)
+        {
+            radius = radiusInput;
+            circumference = GetCircumference();
+
+         }
+
+        public string FormattedNumber1
+        {
+            get
+            {
+                return FormattedNumber1;
+            }
+
+            set
+            {
+                FormattedNumber = value;
+            }
+        }
+
+        public string FormattedArea1
+        {
+           
+        }
+
+        public int Radius1
+        {
+            get
+            {
+                return Radius;
+            }
+
+            set
+            {
+                Radius = value;
+            }
+        }
+
+        public string FormatedCircumference1
+        {
+            get
+            {
+                return FormatedCircumference;
+            }
+
+            set
+            {
+                FormatedCircumference = value;
+            }
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+                 
+            }
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome Message
+            Console.WriteLine("\nWelcome To Radius!");
+            Console.WriteLine();
+
+
+            // Declare Variables
+            string str = new string('*', 20);
+            CircleClass Circle1 = new CircleClass();
+            Circle1.Radius1 = 0;
+            Circle1.Circumference1 = 0;
+            Circle1.Area1 = 0;
+            Circle1.FormatedCircumference1 = 0;
+            Circle1.FormattedNumber1 = 0;
+            Circle1.FormattedArea1 = 0;
+
+            //Math functions
+            double radius = double.Parse(Console.ReadLine());
+            double pi = Math.PI;
+            //double area = 2 * pi * Radius1;
+
+            //Request from User
+
+            Console.Write("Enter the radius");
+            Circle1.Radius1 = int.Parse(Console.ReadLine());
+
+            // Contin
+            //while (true)
+            //{
+            //    Console.WriteLine("\nEnter the RADIUS: ");
+
+            //}
+        }
+    }
+}
+
